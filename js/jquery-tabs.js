4968a54c48da9fe0a7a47bd18b447094bdd2f9d6
Lyxiae <43345211+Lyxiae@users.noreply.github.com>
2020-06-29 15:26:09 +0200

    Initial files

---
+++ js/jquery-tabs.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $( function() {
+    $( "#tabs" ).tabs({
+      collapsible: true,
+      hide: { effect: "fade", duration: 300, easing: "easeInOutQuart" },
+      show: { effect: "fade", duration: 300, easing: "easeInOutQuart" }
+    });
+  } );
+
+
+});
