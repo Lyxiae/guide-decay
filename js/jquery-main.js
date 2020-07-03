61b6699ebb57d74d77e4da888999a0dbb962d5f6
Marjolaine Robin <robin.marjolaine@gmail.com>
2020-07-03 22:30:41 +0200

    Update guide jusqu'à triade + milice

--- js/jquery-main.js
+++ js/jquery-main.js
@@ -8,6 +8,68 @@ $( function() {
     });
 } );
 
+$( function() {
+    $( "#accordion-lombardi" ).accordion({
+    collapsible: true,
+    active: false
+    });
+} );
+
+$( function() {
+    $( "#accordion-triade" ).accordion({
+    collapsible: true,
+    active: false
+    });
+} );
+
+$( function() {
+    $( "#accordion-yakuza" ).accordion({
+    collapsible: true,
+    active: false
+    });
+} );
+
+$( function() {
+    $( "#accordion-orga" ).accordion({
+    collapsible: true,
+    active: false
+    });
+} );
+
+$( function() {
+    $( "#accordion-cartel" ).accordion({
+    collapsible: true,
+    active: false
+    });
+} );
+
+$( function() {
+    $( "#accordion-alliance" ).accordion({
+    collapsible: true,
+    active: false
+    });
+} );
+
+$( function() {
+    $( "#accordion-underground" ).accordion({
+    collapsible: true,
+    active: false
+    });
+} );
+
+$( function() {
+    $( "#accordion-milice" ).accordion({
+    collapsible: true,
+    active: false
+    });
+} );
+
+$( function() {
+    $( "#accordion-eglise" ).accordion({
+    collapsible: true,
+    active: false
+    });
+} );
 /**
  * TABS
  */
